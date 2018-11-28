5100966a9a4b645a49d62dc50585a705eae3a904
HarshAraple <araple71@gmail.com>
2018-11-29 02:29:59 +0530

    Android Crud Operation
    Using Multiple Table with sqlite database

---
+++ app/src/main/java/com/student_sqlite/harsharaple/student_sqlite/MainActivity.java
@@ -0,0 +1,13 @@
+package com.student_sqlite.harsharaple.student_sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
